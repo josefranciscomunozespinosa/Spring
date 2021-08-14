840188e77430ad6b62dc0c91720ef996048f0e2a
Jose Francisco <josefranciscomunozespinosa@gmail.com>
2021-08-14 20:29:25 +0200

    Ejemplo REST con tests unit

---
+++ spring-boot-rest-services-with-unit-and-integration-tests/src/main/java/es/eoi/springboot/StudentServicesApplication.java
@@ -0,0 +1,12 @@
+package es.eoi.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServicesApplication.class, args);
+	}
+}
---
+++ spring-boot-rest-services-with-unit-and-integration-tests/src/main/java/es/eoi/springboot/service/StudentService.java
@@ -0,0 +1,102 @@
+package es.eoi.springboot.service;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import es.eoi.springboot.model.Course;
+import org.springframework.stereotype.Component;
+
+import es.eoi.springboot.model.Student;
+
+@Component
+public class StudentService {
+
+	private static List<Student> students = new ArrayList<>();
+
+	static {
+		//Initialize Data
+		Course course1 = new Course("Course1", "Spring", "10 Steps", Arrays
+				.asList("Learn Maven", "Import Project", "First Example",
+						"Second Example"));
+		Course course2 = new Course("Course2", "Spring MVC", "10 Examples",
+				Arrays.asList("Learn Maven", "Import Project", "First Example",
+						"Second Example"));
+		Course course3 = new Course("Course3", "Spring Boot", "6K Students",
+				Arrays.asList("Learn Maven", "Learn Spring",
+						"Learn Spring MVC", "First Example", "Second Example"));
+		Course course4 = new Course("Course4", "Maven",
+				"Most popular maven course on internet!", Arrays.asList(
+						"Pom.xml", "Build Life Cycle", "Parent POM",
+						"Importing into Eclipse"));
+
+		Student student1 = new Student("Student1", "Jose Francisco",
+				"Hiker, Programmer and Architect", new ArrayList<>(Arrays
+						.asList(course1, course2, course3, course4)));
+
+		Student student2 = new Student("Student2", "Maria",
+				"Hiker, Programmer and Architect", new ArrayList<>(Arrays
+						.asList(course1, course2, course3, course4)));
+
+		students.add(student1);
+		students.add(student2);
+	}
+
+	public List<Student> retrieveAllStudents() {
+		return students;
+	}
+
+	public Student retrieveStudent(String studentId) {
+		for (Student student : students) {
+			if (student.getId().equals(studentId)) {
+				return student;
+			}
+		}
+		return null;
+	}
+
+	public List<Course> retrieveCourses(String studentId) {
+		Student student = retrieveStudent(studentId);
+
+		if (student == null) {
+			return null;
+		}
+
+		return student.getCourses();
+	}
+
+	public Course retrieveCourse(String studentId, String courseId) {
+		Student student = retrieveStudent(studentId);
+
+		if (student == null) {
+			return null;
+		}
+
+		for (Course course : student.getCourses()) {
+			if (course.getId().equals(courseId)) {
+				return course;
+			}
+		}
+
+		return null;
+	}
+
+	private SecureRandom random = new SecureRandom();
+
+	public Course addCourse(String studentId, Course course) {
+		Student student = retrieveStudent(studentId);
+
+		if (student == null) {
+			return null;
+		}
+
+		String randomId = new BigInteger(130, random).toString(32);
+		course.setId(randomId);
+
+		student.getCourses().add(course);
+
+		return course;
+	}
+}
