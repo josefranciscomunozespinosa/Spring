17a3c85a43376d93adf53ab32a228229985445f4
Jose Francisco <josefranciscomunozespinosa@gmail.com>
2021-08-14 15:51:13 +0200

    Ejemplo Jdbc con h2

---
+++ spring-boot-2-jdbc-with-h2/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into student
+values(10001,'Jose', 'E1234567');
+
+insert into student
+values(10002,'Maria Angeles', 'A1234568');
---
+++ spring-boot-2-jdbc-with-h2/src/test/java/es/eoi/springboot/jdbc/h2/example/SpringBootJdbcWithH2ApplicationTests.java
@@ -0,0 +1,16 @@
+package es.eoi.springboot.jdbc.h2.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootJdbcWithH2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
